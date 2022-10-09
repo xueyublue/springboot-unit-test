46e4929781265391740d8ad32fb6de206d0fdd5a
Xue Yu <xueyublue@gmail.com>
2022-10-09 14:43:42 +0800

    Added OpenAPI

--- src/main/java/sg/darren/unittest/UnitTestApplication.java
+++ src/main/java/sg/darren/unittest/UnitTestApplication.java
@@ -1,13 +1,19 @@
 package sg.darren.unittest;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(
+        title = "Spring boot Unit Test API",
+		version = "1.0"
+))
 public class UnitTestApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UnitTestApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(UnitTestApplication.class, args);
+    }
 
 }
--- src/main/java/sg/darren/unittest/controller/EmployeeController.java
+++ src/main/java/sg/darren/unittest/controller/EmployeeController.java
@@ -1,4 +1,22 @@
 package sg.darren.unittest.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import sg.darren.unittest.model.Employee;
+
+@RestController
+@RequestMapping("/employees")
 public class EmployeeController {
+
+    @GetMapping("/{id}")
+    public Employee getEmployee(@RequestParam Long id) {
+        return Employee.builder()
+                .id(id)
+                .firstName("Spring-boot")
+                .lastName("Unit Test")
+                .email("spring-boot.unit.test@.mail.com")
+                .build();
+    }
 }
